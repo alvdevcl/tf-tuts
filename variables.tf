2f2e37e038c92fa29941a473c687a788e57bc679
Alveo <alveo@Lolucodes-MacBook-Pro.local>
2024-04-27 14:01:59 +0100

    changed ami

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "ami" {
    type        = string
    description = "Ubuntu AMI ID in N. Virginia Region"
-   default     = "ami-065deacbcaac64cf2"
+   default     = "ami-06dd34cd4bdc97b1d"
 }
 
 variable "instance_type" {
