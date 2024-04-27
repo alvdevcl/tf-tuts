753abbdb40f01fc3c1f0dbf868d200c5d5f8d4a3
Alveo <alveo@Lolucodes-MacBook-Pro.local>
2024-04-27 17:30:23 +0100

    terraform providers

--- provider.tf
+++ provider.tf
@@ -1,8 +1,45 @@
 terraform {
- required_providers {
-   aws = {
-     source  = "hashicorp/aws"
-     version = "~> 4.18.0"
-   }
- }
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.10.1"
+    }
+    bcrypt = {
+      source  = "viktorradnai/bcrypt"
+      version = ">= 0.1.2"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.22.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.3.2"
+    }
+    tls = {
+      version = ">= 3.1.0"
+      source  = "hashicorp/tls"
+    }
+
+    http = {
+      version = ">= 2.1.0"
+      source  = "hashicorp/http"
+    }
+  }
+  #  backend "s3" {
+  #    # Replace this with your bucket name!
+  #    bucket         = "ops360primecloud-tfstate-poc"
+  #    key            = "setup/terraform.tfstate.dev"
+  #    region         = "us-east-1"
+
+  #    # Replace this with your DynamoDB table name!
+  #    dynamodb_table = "nvirginia_tfstate_locks_dev"
+  #    encrypt        = true
+  #  }
 }
